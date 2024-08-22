72a87f6f254e3e0e37f6e55737de977b5641f4e6
aixa <aixaberriocontacto@gmail.com>
2024-08-22 18:43:55 -0300

    agrego fetch de datos

--- carrito.js
+++ carrito.js
@@ -59,3 +59,5 @@ const sumarDelCarrito = (id) => {
     renderProducts(carrito);
   }
 };
+
+renderProducts(carrito);
--- index.js
+++ index.js
@@ -1,66 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const predefinedProducts = [
-    {
-      id: "1",
-      title: "BMW",
-      price: 200,
-      Category: "Autos",
-      image: "../resources/auto.jpg",
-      description: "-",
-      rating: { rate: 4.5, count: 120 },
-    },
-    {
-      id: "2",
-      title: "Nissan",
-      price: 500,
-      Category: "Camionetas",
-      image: "../resources/auto.jpg",
-      description: "-",
-      rating: { rate: 4.5, count: 120 },
-    },
-    {
-      id: "3",
-      title: "Taza",
-      price: 100,
-      Category: "Repuestos",
-      image: "../resources/auto.jpg",
-      description: "-",
-      rating: { rate: 4.5, count: 120 },
-    },
-    {
-      id: "4",
-      title: "Honda",
-      price: 600,
-      Category: "Autos",
-      image: "../resources/auto.jpg",
-      description: "-",
-      rating: { rate: 4.5, count: 120 },
-    },
-    {
-      id: "5",
-      title: "Amarok",
-      price: 100,
-      Category: "Camionetas",
-      image: "../resources/auto.jpg",
-      description: "-",
-      rating: { rate: 4.5, count: 120 },
-    },
-    {
-      id: "6",
-      title: "faro",
-      price: 150,
-      Category: "Repuestos",
-      image: "../resources/auto.jpg",
-      description: "-",
-      rating: { rate: 4.5, count: 120 },
-    },
-  ];
-
+  let predefinedProducts = [];
   let storedProducts = JSON.parse(localStorage.getItem("products")) || [];
-
-  let products = [...predefinedProducts, ...storedProducts];
-
   let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  let products = [];
 
   const renderProducts = (arrayProducts) => {
     const containerProducts = document.getElementById("containerProducts");
@@ -68,24 +10,48 @@ document.addEventListener("DOMContentLoaded", () => {
     arrayProducts.forEach((product) => {
       let productCard = document.createElement("div");
       productCard.innerHTML = `
-        <img src="${product.image}" />
-        <h2>${product.title}</h2>
-        <h3>${product.price} USD</h3>
-        <button onclick="agregarAlCarrito('${product.id}')">Agregar al Carrito</button>`;
+          <img src="${product.image}" />
+          <h2>${product.title}</h2>
+          <h3>${product.price} USD</h3>
+          <button class="add-to-cart-btn" data-id="${product.id}">Agregar al Carrito</button>`;
       productCard.className = "card";
       containerProducts.appendChild(productCard);
     });
-  };
 
-  renderProducts(products);
+    // Agregar los event listeners para todos los botones de "Agregar al Carrito"
+    const buttons = document.querySelectorAll(".add-to-cart-btn");
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        agregarAlCarrito(button.dataset.id);
+      });
+    });
+  };
 
   const agregarAlCarrito = (id) => {
     let product = products.find((elemento) => elemento.id === id);
     let productEnElCarrito = carrito.find((elemento) => elemento.id === id);
     if (productEnElCarrito) {
       productEnElCarrito.quanty += 1;
+      Toastify({
+        text: "Agregaste 1 más al carrito",
+        duration: 3000,
+        gravity: "bottom",
+        position: "center",
+        style: {
+          background: "linear-gradient(to right, black, grey)",
+        },
+      }).showToast();
     } else {
       carrito.push({ ...product, quanty: 1 });
+      Toastify({
+        text: "Producto agregado al carrito",
+        duration: 3000,
+        gravity: "bottom",
+        position: "center",
+        style: {
+          background: "linear-gradient(to right, black, grey)",
+        },
+      }).showToast();
     }
     localStorage.setItem("carrito", JSON.stringify(carrito));
   };
@@ -100,35 +66,13 @@ document.addEventListener("DOMContentLoaded", () => {
       renderProducts(arrayFiltrado);
     });
   }
-});
 
-const agregarAlCarrito = (id) => {
-  let products = JSON.parse(localStorage.getItem("products")) || [];
-  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-  let product = products.find((elemento) => elemento.id === id);
-  let productEnElCarrito = carrito.find((elemento) => elemento.id === id);
-  if (productEnElCarrito) {
-    productEnElCarrito.quanty += 1;
-    Toastify({
-      text: "Agregaste 1 más al carrito",
-      duration: 3000,
-      gravity: "bottom",
-      position: "center",
-      style: {
-        background: "linear-gradient(to right, black, grey)",
-      },
-    }).showToast();
-  } else {
-    carrito.push({ ...product, quanty: 1 });
-    Toastify({
-      text: "Producto agregado al carrito",
-      duration: 3000,
-      gravity: "bottom",
-      position: "center",
-      style: {
-        background: "linear-gradient(to right, black, grey)",
-      },
-    }).showToast();
-  }
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-};
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      predefinedProducts = data;
+      products = [...predefinedProducts, ...storedProducts];
+      renderProducts(products);
+    })
+    .catch((error) => console.error("Error al cargar los productos:", error));
+});
